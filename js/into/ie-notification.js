a318f464aa77fa8a4069521c25c34139c3e0512d
Your Name <you@abc.net>
2021-03-20 13:31:37 +0600

    first commit

---
+++ js/into/ie-notification.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#ieNotificationModal').on('show.bs.modal',
+        function (event) {
+            var relatedTarget = $(event.relatedTarget);
+            var message = relatedTarget.data("message");
+            
+            var modal = $(this);
+            modal.find(".modal-body #message").html(message);
+        });
+});
